981e3ae3189e27371ec6012c0f43444178802ec6
Francisco <fnaveira11@gmail.com>
2021-05-19 15:24:29 -0400

    commit tree 513f63d5039cd741afbaabe96b0e78abb5f019fe

--- lib/person.rb
+++ lib/person.rb
@@ -50,32 +50,35 @@ class Person
     end
 
     def take_bath
-        @hygiene= @hygiene + 4
-        # self.hygiene + 4
+        self.hygiene += 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        @hygiene= @hygiene -3
-        @happiness= @happiness + 2
+        self.hygiene -= 3
+        self.happiness += 2
         "♪ another one bites the dust ♫"
     end
 
     def call_friend (friend)
-        friend.happiness = friend.happiness + 3
-        @happiness= @happiness + 3
+        # var = friend.happiness
+        # var += 3
+        # friend.happiness = var
+        # friend.happiness = friend.happiness + 3
+        friend.happiness += 3
 
+        self.happiness += 3
         "Hi #{friend.name}! It's #{@name}. How are you?"
     end
 
     def start_conversation(person,topic)
         if topic == 'politics'
-            @happiness= @happiness -2
-            person.happiness = person.happiness -2
+            self.happiness -= 2
+            person.happiness -= 2
             "blah blah partisan blah lobbyist"
         elsif topic == 'weather'
-            @happiness= @happiness +1
-            person.happiness = person.happiness +1
+            self.happiness += 1
+            person.happiness += 1
             "blah blah sun blah rain"
         else
             "blah blah blah blah blah"
